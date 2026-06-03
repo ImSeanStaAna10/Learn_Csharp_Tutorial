2e17a5d97045824a25fd39663e1251f3313d8580
ImSeanStaAna10 <seanstaana0510@gmail.com>
2026-06-03 21:03:30 +0800

    updated

--- LINQ and LAMBDA/Program.cs
+++ LINQ and LAMBDA/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -14,16 +13,18 @@ namespace LINQ_and_LAMBDA
 {
     class Program
     {
-        public static void Main(string[]args)
+        public static void Main(string[] args)
         {
 
             // print even numbers only 
             var numbers = new[] { 5, 6, 2, 9, 1 };
+
             #region -- first sample -- 
 
             #region -- FOR EACH AND IF-ELSE
             //using Loop and if-else
             var evenNumbers = new List<int>(); // empty list dito ilalagay yung mga even numbers
+
             foreach (var number in numbers)
             {
                 if (number % 2 == 0)
@@ -31,20 +32,21 @@ namespace LINQ_and_LAMBDA
                     evenNumbers.Add(number);
                 }
             }
+
             Console.WriteLine("FOR EACH AND IF");
+
             foreach (var even in evenNumbers)
             {
                 Console.WriteLine(even);
             }
 
-
-                
             Console.WriteLine("**********************");
 
             #endregion
 
             #region -- LINQ --
             Console.WriteLine("LINQ");
+
             //USING LINQ
             /*
               * FROM bawat "number" sa loob ng "numbers"
@@ -60,7 +62,12 @@ namespace LINQ_and_LAMBDA
              -> Yung mga numbers lang na pumasa sa condition
                 ang isasama sa final result/output
              */
-            var linqEvenNumbers = from number in numbers where number % 2 == 0 select number;
+
+            var linqEvenNumbers =
+                from number in numbers
+                where number % 2 == 0
+                select number;
+
             foreach (var even in linqEvenNumbers)
             {
                 Console.WriteLine(even);
@@ -69,6 +76,7 @@ namespace LINQ_and_LAMBDA
             #endregion
 
             #region -- LAMBDA EXPRESSION --
+
             /*
             LINQ + Lambda (Where filter)
 
@@ -92,14 +100,19 @@ namespace LINQ_and_LAMBDA
             */
 
             Console.WriteLine("************** LAMBDA EXPRESSION ******************");
-            var lambdaEvenNumbers = numbers.Where(number => number % 2 == 0);
+
+            var lambdaEvenNumbers =
+                numbers.Where(number => number % 2 == 0);
+
             foreach (var even in lambdaEvenNumbers)
             {
                 Console.WriteLine(even);
             }
+
             #endregion
 
             #endregion
+
 
             #region -- WITH ORDER AND TRANSFORMATION OF DATA -- 
 
@@ -110,60 +123,69 @@ namespace LINQ_and_LAMBDA
             var mixedNumbers = new[] { 3, 5, 6, 9 };
 
             #region -- loopings and Conditional stsment
-            var numberContainer = new List<int>(); 
+
+            var numberContainer = new List<int>();
+
             Console.WriteLine("------------- LOOPINGS AND CONDITIONAL ----------------");
 
             // filter
             foreach (var mixNum in mixedNumbers)
             {
-                if (mixNum < 9 )
+                if (mixNum < 9)
                 {
                     numberContainer.Add(mixNum);
                 }
             }
 
-            for (int i = numberContainer.Count - 1 ; i>=0; i--)
+            for (int i = numberContainer.Count - 1; i >= 0; i--)
             {
                 Console.WriteLine($"NUMERO: {numberContainer[i]}");
             }
 
-         
             #endregion
 
             #region -- LINQ --
-            //Linq
-            var linqQuery = from number in mixedNumbers
-                            where number <  9
-                            orderby number descending
-                            select $"NUMERO: {number}";
+
+            var linqQuery =
+                from number in mixedNumbers
+                where number < 9
+                orderby number descending
+                select $"NUMERO: {number}";
 
             Console.WriteLine("------------- LINQ ----------------");
+
             foreach (var num in linqQuery)
             {
                 Console.WriteLine(num);
             }
+
             #endregion
 
             #region -- LAMBDA -- 
-            var lambdaQuery = mixedNumbers
-                            .Where(number => number < 9)
-                            .OrderByDescending(number => number)
-                            .Select(number => $"NUMERO:{number}");
+
+            var lambdaQuery =
+                mixedNumbers
+                    .Where(number => number < 9)
+                    .OrderByDescending(number => number)
+                    .Select(number => $"NUMERO:{number}");
+
             Console.WriteLine("------------- LAMBDA ----------------");
+
             foreach (var num in lambdaQuery)
             {
                 Console.WriteLine(num);
             }
 
-
             #endregion
 
             #endregion
+
 
             #region -- JOINS --
 
             Console.WriteLine("********* JOINS *******************");
             Console.WriteLine();
+
             var students = new List<Student>()
             {
                new Student("Luffy",22,1),
@@ -175,49 +197,51 @@ namespace LINQ_and_LAMBDA
             {
                 new Section(1, "Pirate"),
                 new Section(2 , "revolutionary")
-
             };
 
             #region -- LINQ -- 
+
             Console.WriteLine("**************** LINQ VERSION *******************");
-            //linq
-            var query = from section in sections // Kunin ang bawat section mula sa sections.
-                        join student in students on section.Grade equals student.Grade // I-match ang bawat section sa students. Pag pareho ang Grade ng section and student, pagsamahin sila.  ON means "condition of the join"
-                        where student.Age > 20 // Kunin lang ang students na mas mababa sa 25 years old.
-                        orderby student.Age, student.Grade // I-sort ayon sa Age, kapag pareho ang Age, saka gamitin ang Grade.
-                        select new // Gumawa ng bagong object.
-                        {
-                            section.SectionName, // anonymous object
-                            StudentName = student.Name,
-                        };
+
+            var query =
+                from section in sections
+                join student in students on section.Grade equals student.Grade
+                where student.Age > 20
+                orderby student.Age, student.Grade
+                select new
+                {
+                    section.SectionName,
+                    StudentName = student.Name,
+                };
 
             foreach (var sectionAndStudent in query)
             {
                 Console.WriteLine($"Section: {sectionAndStudent.SectionName}, Student Name: {sectionAndStudent.StudentName}");
             }
+
             #endregion
-
-
 
 
             #region -- LAMBDA --
 
             Console.WriteLine("**************** LAMBDA VERSION ***********************");
-            var lambdaQuerys = sections // Magsimula sa listahan ng sections
-                             .Join( // I-join ang students sa bawat section gamit ang Grade
-                              students.Where(student => student.Age > 20), // Isama lang ang students na age below 25
-                              section => section.Grade, // Grade ng section
-                              student => student.Grade, // Grade ng student
-                              (section, student) => new // Kapag pareho ang Grade, pagsamahin ang section at student
-                              {
-                                  section.SectionName,
-                                  StudentName = student.Name,
-                                  student.Age,
-                                  student.Grade
-                                  }
-                              )
-                              .OrderBy(student => student.Age) // I-sort ayon sa Age
-                              .ThenBy(student => student.Grade); // Kapag pareho ang Age, Grade naman ang basehan   
+
+            var lambdaQuerys =
+                sections
+                    .Join(
+                        students.Where(student => student.Age > 20),
+                        section => section.Grade,
+                        student => student.Grade,
+                        (section, student) => new
+                        {
+                            section.SectionName,
+                            StudentName = student.Name,
+                            student.Age,
+                            student.Grade
+                        }
+                    )
+                    .OrderBy(student => student.Age)
+                    .ThenBy(student => student.Grade);
 
             foreach (var sectionAndStudent in lambdaQuerys)
             {
@@ -227,10 +251,11 @@ namespace LINQ_and_LAMBDA
             #endregion
             #endregion
 
+
             #region -- GROUPINGS --
 
-
             Console.WriteLine("********* GROUPINGS *******************");
+
             var cars = new List<Cars>()
             {
                 new Cars("Toyota", "Red", "Corolla" , 2010),
@@ -242,92 +267,209 @@ namespace LINQ_and_LAMBDA
             #region -- LINQ -- 
 
             Console.WriteLine("********* LINQ *******************");
-            var groupLinq = from car in cars
-                            group car by car.modelYear into carModelYear
-                            select new carGroup{ ModelYear = carModelYear.Key, Count = carModelYear.Count() };
+
+            var groupLinq =
+                from car in cars
+                group car by car.modelYear into carModelYear
+                select new carGroup
+                {
+                    ModelYear = carModelYear.Key,
+                    Count = carModelYear.Count()
+                };
 
             foreach (var carModelYear in groupLinq)
             {
                 Console.WriteLine($"Year: {carModelYear.ModelYear}, Count: {carModelYear.Count}");
             }
 
+            #endregion
+
+
             #region -- LAMBDA --
+
             Console.WriteLine("********* LINQ *******************");
-          var lambdaGroup = cars.GroupBy(car => car.modelYear, car  =>car) // I-group ang mga cars base sa modelYear
-                                .Select(group => new carGroup // Para sa bawat grupo, gumawa ng bagong carGroup object
-                                {
-                                    ModelYear = group.Key, // Ang ModelYear ay ang key ng grupo (modelYear)
-                                    Count = group.Count() // Ang Count ay bilang ng mga cars sa grupo
-                                });
+
+            var lambdaGroup =
+                cars.GroupBy(car => car.modelYear)
+                    .Select(group => new carGroup
+                    {
+                        ModelYear = group.Key,
+                        Count = group.Count()
+                    });
 
             foreach (var carModelYear in lambdaGroup)
             {
                 Console.WriteLine($"Year: {carModelYear.ModelYear}, Count: {carModelYear.Count}");
             }
-            #endregion
 
             #endregion
+            #endregion
 
 
+            #region -- LAMBDA ONLY FUNCTION / BASIC AGGREGATION --
+
+            Console.WriteLine("********* LAMBDA ONLY FUNCTIONS *******************");
+
+            var sampleNumber = Enumerable.Range(1, 10);
+            Console.WriteLine("Sample Number: " + string.Join(", ", sampleNumber));
+
+            Console.WriteLine("------- AVERAGE FUNCTION -------");
+
+            var average = sampleNumber.Average();
+            var num1to5average = sampleNumber.Where(num => num <= 5).Average();
+
+            Console.WriteLine($"Average {average}");
+            Console.WriteLine($"Average number 1 to 5: {num1to5average}");
+
+            var count = sampleNumber.Count();
+            var min = sampleNumber.Min();
+            var max = sampleNumber.Max();
+            var summation = sampleNumber.Sum();
+
+            Console.WriteLine($"COUNT: {count}, MAX: {max}, MIN: {min}, SUMMATION: {summation}");
 
             #endregion
+
+
+            #region -- CONVERSION --
+
+            Console.WriteLine("********* CONVERSION *******************");
+
+            var sampleNumber2 = Enumerable.Range(1, 15);
+
+            var list = new List<int>(sampleNumber2);
+            var enumValue = list.AsEnumerable();
+            var backtoList = enumValue.ToList();
+
+            list = (from numbersSamp in sampleNumber2 where numbersSamp >= 5 select numbersSamp).ToList();
+
+            list.Add(16);
+
+            Console.WriteLine("CONVERSION: " + string.Join(",", list));
+
+            #endregion
+
+
+            #region -- BASIC ELEMENT ACCESS -- 
+
+            Console.WriteLine("------- BASIC ELEMENT ACCESS -------");
+
+            var numbersSamp2 = Enumerable.Range(1, 10);
+
+            var firstNumber = numbersSamp2.FirstOrDefault();
+            var lastNumber = numbersSamp2.LastOrDefault();
+            var firstEvenNumber = numbersSamp2.FirstOrDefault(n => n % 2 == 0);
+
+            var firstWithoutDefault = numbersSamp2.First();
+            var lastWithoutDefault = numbersSamp2.Last();
+
+            Console.WriteLine($"First Number: {firstNumber}, Last Number: {lastNumber}, First Even Number: {firstEvenNumber}");
+            Console.WriteLine($"First without default: {firstWithoutDefault}, Last without default: {lastWithoutDefault}");
+
+            #endregion
+
+
+            #region -- PARTITIONING --
+
+            Console.WriteLine("------- PARTITIONING -------");
+
+            var sampleNumber3 = Enumerable.Range(1, 10);
+
+            var numbersSkip5 = sampleNumber3.Skip(5);
+            var numbersSkipWhile = sampleNumber3.SkipWhile(n => n < 5);
+
+            var numbersTake5 = sampleNumber3.Take(5);
+            var numberTakeWhile = sampleNumber3.TakeWhile(n => n < 5);
+
+            Console.WriteLine($"Skip5: {string.Join(",", numbersSkip5)}");
+            Console.WriteLine($"SkipWhile < 5: {string.Join(",", numbersSkipWhile)}");
+            Console.WriteLine($"Take5: {string.Join(",", numbersTake5)}");
+            Console.WriteLine($"TakeWhile < 5: {string.Join(",", numberTakeWhile)}");
+
+            #endregion
+
+
+            #region -- SET  --
+
+            Console.WriteLine("------- SET -------");
+
+            var repeatedNumbers = new[] { 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10 };
+            var distinctNumbers = repeatedNumbers.Distinct();
+
+            Console.WriteLine($"Distinct Numbers: {string.Join(",", distinctNumbers)}");
+
+            var sampleNumbers4 = Enumerable.Range(1, 10);
+            var excludeNumbers = new[] { 5, 6, 7 };
+            var newNumbers = sampleNumbers4.Except(excludeNumbers);
+
+            Console.WriteLine($"Exclude : {string.Join(",", newNumbers)}");
+
+            var sampleNumbers5 = Enumerable.Range(1, 10);
+            var checkerNumbers = new int[] { 1, 2, 3 };
+            var intersectNumbers = sampleNumbers5.Intersect(checkerNumbers);
+
+            Console.WriteLine($"Intersect: {string.Join(",", intersectNumbers)}");
+
+            var sampleNumbers6 = Enumerable.Range(1, 10);
+            var appendNumbers = new[] { 10, 11, 12, 13, 14, 15 };
+            var unionNumbers = sampleNumbers6.Union(appendNumbers);
+
+            Console.WriteLine($"Union: {string.Join(",", unionNumbers)}");
         }
+
+            #endregion
+
     }
-
-    #region -- FOR DEMO PURPOSES --
-
-    public class Student
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public int Grade { get; set; }
-
-        public Student(string name, int age, int grade) // constuctor
-        {
-            Name = name;
-            Age = age;
-            Grade = grade;
-        }
-    }
-    public class Section
-    {
-
-        public int Grade { get; set; }
-        public string SectionName { get; set; } // constructor
-     
-
-        public Section(int grade, string sectionName)
-        {
-            Grade = grade;
-            SectionName = sectionName;
-        
-        }
-    }
-
-    public class Cars
-    {
-        public string Brand { get; set; }
-        public string Color { get; set; }
-        public string carName { get; set; }
-
-       public int modelYear { get; set; }
-
-        public  Cars(string brand, string color, string name, int model) // constructor
-        {
-            Brand = brand;
-            Color = color;
-            carName = name;
-            modelYear = model;
-        }
-
-    };
-
-    public class carGroup
-    {
-        public int ModelYear { get; set; }
-        public int Count { get; set; }
-    }
-
-   
-    #endregion
 }
+
+#region -- FOR DEMO PURPOSES --
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Grade { get; set; }
+
+    public Student(string name, int age, int grade)
+    {
+        Name = name;
+        Age = age;
+        Grade = grade;
+    }
+}
+
+public class Section
+{
+    public int Grade { get; set; }
+    public string SectionName { get; set; }
+
+    public Section(int grade, string sectionName)
+    {
+        Grade = grade;
+        SectionName = sectionName;
+    }
+}
+
+public class Cars
+{
+    public string Brand { get; set; }
+    public string Color { get; set; }
+    public string carName { get; set; }
+    public int modelYear { get; set; }
+
+    public Cars(string brand, string color, string name, int model)
+    {
+        Brand = brand;
+        Color = color;
+        carName = name;
+        modelYear = model;
+    }
+}
+
+public class carGroup
+{
+    public int ModelYear { get; set; }
+    public int Count { get; set; }
+}
+
+    #endregion
